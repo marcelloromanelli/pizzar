a8badaefe632dff6811ee709004bc1dfd60caec0
Marcello Romanelli <marcello.romanelli@getyourguide.com>
2023-03-10 21:44:26 +0100

    Improve style

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
-// Helper function to convert degrees to radians
-function toRadians(degrees) {
-  return (degrees * Math.PI) / 180;
-}
+import { getAngle, calculateDistance, getCurrentPosition } from "./utils.js";
 
-// Function to calculate the distance between two points using the Haversine formula
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Earth's radius in km
-  const dLat = toRadians(lat2 - lat1);
-  const dLon = toRadians(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(toRadians(lat1)) *
-      Math.cos(toRadians(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  const distance = R * c;
-  return distance;
-}
-
-function getAngle(alpha, target_lat, target_lon, current_lat, current_lon) {
-  // Calculate the bearing angle from your current location to the target location
-  const delta_lon = target_lon - current_lon;
-  const y = Math.sin(delta_lon) * Math.cos(target_lat);
-  const x =
-    Math.cos(current_lat) * Math.sin(target_lat) -
-    Math.sin(current_lat) * Math.cos(target_lat) * Math.cos(delta_lon);
-  let bearing = Math.atan2(y, x);
-  bearing = bearing * (180 / Math.PI);
-
-  // Adjust the bearing angle to take into account your device's compass heading
-  let angle = bearing - alpha;
-  if (angle < 0) {
-    angle += 360;
-  }
-
-  console.log(
-    "Angle direction from current location to target location:",
-    angle
-  );
-  return angle;
-}
-
-// Get the current position using the Geolocation API
-navigator.geolocation.getCurrentPosition((position) => {
-  const myLat = position.coords.latitude;
-  const myLng = position.coords.longitude;
-
-  console.log(position.coords);
-
+window.onload = async () => {
   // Closest pizza place
   const pizzaLat = 52.53291;
-  const pizzaLng = 13.40868;
+  const pizzaLong = 13.40868;
 
-  // Calculate the distance between the two points
-  const distance = calculateDistance(myLat, myLng, pizzaLat, pizzaLng);
-  document.getElementById("distance").innerHTML = `${Math.round(distance * 100) / 100} KM`;
-
-  ////////////////////
-  /// MOVE OUT
-  ///////////////////
+  const { lat, long } = await getCurrentPosition();
+  const distance = calculateDistance(lat, long, pizzaLat, pizzaLong);
+  document.getElementById("distance").innerHTML = `${
+    Math.round(distance * 10) / 10
+  } KM`;
 
   // Get the needle element
   const needle = document.getElementById("needle");
@@ -74,8 +24,8 @@ navigator.geolocation.getCurrentPosition((position) => {
     const beta = event.beta;
     const gamma = event.gamma;
 
-    const angle = getAngle(alpha, pizzaLat, pizzaLng, myLat, myLng);
+    const angle = getAngle(alpha, pizzaLat, pizzaLong, lat, long);
     // Rotate the needle based on the alpha value
     needle.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
   }
-});
+};
---
+++ utils.js
@@ -0,0 +1,60 @@
+// Helper function to convert degrees to radians
+function toRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+// Function to calculate the distance between two points using the Haversine formula
+export function calculateDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Earth's radius in km
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) *
+      Math.cos(toRadians(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c;
+  return distance;
+}
+
+export function getAngle(
+  alpha,
+  target_lat,
+  target_lon,
+  current_lat,
+  current_lon
+) {
+  // Calculate the bearing angle from your current location to the target location
+  const delta_lon = target_lon - current_lon;
+  const y = Math.sin(delta_lon) * Math.cos(target_lat);
+  const x =
+    Math.cos(current_lat) * Math.sin(target_lat) -
+    Math.sin(current_lat) * Math.cos(target_lat) * Math.cos(delta_lon);
+  let bearing = Math.atan2(y, x);
+  bearing = bearing * (180 / Math.PI);
+
+  // Adjust the bearing angle to take into account your device's compass heading
+  let angle = bearing - alpha;
+  if (angle < 0) {
+    angle += 360;
+  }
+
+  console.log(
+    "Angle direction from current location to target location:",
+    angle
+  );
+  return angle;
+}
+
+export async function getCurrentPosition() {
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+
+  return {
+    lat: pos.coords.latitude,
+    long: pos.coords.longitude,
+  };
+};
